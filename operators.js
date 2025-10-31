a635d7d34a60d2d3744d9c27d6d7a44e79cb5864
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-10-31 20:23:04 +0530

    Javascript Operators.

---
+++ operators.js
@@ -0,0 +1,41 @@
+//Arithmetic Operators
+var a=10
+var b=4
+document.writeln("<h2>Arithmetic operators</h2>")
+document.writeln("Result="+(a+b)+"<br>")
+document.writeln("Result="+(a-b)+"<br>")
+document.writeln("Result="+(a*b)+"<br>")
+document.writeln("Result="+(a/b)+"<br>")
+document.writeln("Result="+(a**b)+"<br>")
+document.writeln("Result="+(a%b)+"<br>")
+document.writeln("Result="+(a++)+"<br>")
+document.writeln("Result="+(--a)+"<br>")
+
+//Comparison Operators
+document.writeln("<h2>Coparison operators</h2>")
+a=10
+b=4
+document.writeln("Result="+(a>b)+"<br>")
+document.writeln("Result="+(a<b)+"<br>")
+document.writeln("Result="+(a==b)+"<br>")
+document.writeln("Result="+(a!=b)+"<br>")
+
+//Logical Operators
+document.writeln("<h2>Logical operators</h2>")
+document.writeln("Result="+(!a)+"<br>")
+document.writeln("Result="+(a<b)+"<br>")
+document.writeln("Result="+(a==b)+"<br>")
+document.writeln("Result="+(a!=b)+"<br>")
+
+
+//Assignment Operators
+document.writeln("<h2>Assignment operators</h2>")
+a=20
+document.writeln("A = "+a)
+
+//Conditional Operators
+document.writeln("<h2>Conditinal operators</h2>")
+a=20
+b=10
+max=a>b?a:b
+document.writeln("Max is="+max+"<br>")
